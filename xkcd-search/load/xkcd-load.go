efb491f82dc44f8b8ff54a45cfe824acda6abef5
Cameron Jenkins <cjenknkins144@gmail.com>
2023-12-11 10:29:45 -0500

    Add xkcd project

---
+++ xkcd-search/load/xkcd-load.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+// returns metadata for one comic by number
+func getOne(i int) []byte {
+	url := fmt.Sprintf("https://xkcd.com/%d/info.0.json", i)
+	resp, err := http.Get(url)
+
+	if err != nil {
+		fmt.Fprint(os.Stderr, "Can't read %s\n", err)
+		os.Exit(-1)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Fprintf(os.Stderr, "skipping %d: got %d\n", i, resp.StatusCode)
+		return nil
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Fprint(os.Stderr, "invalid body: %s\n", err)
+		os.Exit(-1)
+	}
+
+	return body
+}
+
+func main() {
+	var (
+		output io.WriteCloser = os.Stdout
+		err    error
+		cnt    int
+		fails  int
+		data   []byte
+	)
+
+	if len(os.Args) > 0 {
+		output, err = os.Create(os.Args[1])
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(-1)
+		}
+
+		defer output.Close()
+	}
+
+	//output will be in JSON arrar
+	//add brackets before and after
+
+	fmt.Fprint(output, "[")
+	defer fmt.Fprint(output, "]")
+
+	for i := 1; fails < 2; i++ {
+		if data = getOne(i); data == nil {
+			fails++
+			continue
+		}
+
+		if cnt > 0 {
+			fmt.Fprint(output, ",") //OB1
+		}
+
+		_, err = io.Copy(output, bytes.NewBuffer(data))
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "stopped: %s\n", err)
+			os.Exit(-1)
+		}
+
+		fails = 0
+		cnt++
+	}
+
+	fmt.Fprintf(os.Stderr, "read %d comics\n", cnt)
+}
