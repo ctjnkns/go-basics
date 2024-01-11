952ff19b6b81c83c8d827de8057f445d385b6649
Cameron Jenkins <cjenknkins144@gmail.com>
2024-01-10 22:42:25 -0500

    add concurrent web server project

---
+++ web-server-concurrent/README.md
@@ -0,0 +1,67 @@
+# web-server
+
+`main.go` starts a basic web server that simulates a store's API. 
+- The database is simulated by a dictionary that maps item names to prices.
+- The API supports calls to list items, read an item, add, update, and delete.
+- The database is initialized with two entries: "shoes": 50 and "socks": 5.
+- There are basic error checks to prevent in each handler. 
+- This version implements mutexes to prevent race conditions in the http server.
+
+List
+
+```shell
+$ curl localhost:8080/list
+shoes: $50.00
+socks: $5.00
+```
+
+Add
+```shell
+$ curl localhost:8080/add?item=ties\&price=A
+invalid price: "A"
+
+$ curl localhost:8080/add?item=ties\&price=13
+added ties with price $13.00
+
+$ curl localhost:8080/list
+shoes: $50.00
+socks: $5.00
+ties: $13.00
+
+$ curl localhost:8080/add?item=tie\&price=50
+duplicate item: "tie"
+```
+
+Update
+```shell
+$ curl localhost:8080/update?item=laces\&price=14
+item not found: "laces"
+
+$ curl localhost:8080/update?item=tie\&price=A
+invalid price: "A"
+
+$ curl localhost:8080/update?item=tie\&price=3
+updated laces with price $3.00
+```
+
+Read
+```shell
+$ curl localhost:8080/read?item=gloves
+item not found "gloves"
+
+$ curl localhost:8080/read?item=tie
+item laces has price $3.00
+```
+
+Delete
+```shell
+$ curl localhost:8080/delete?item=gloves
+item not found "gloves"
+
+$ curl localhost:8080/delete?item=tie
+deleted tie
+
+$ curl localhost:8080/list
+shoes: $50.00
+socks: $3.00
+```
---
+++ web-server-concurrent/main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
+)
+
+type dollars float32 //don't use floats for this in real life
+
+// implement Stringer for formatting prints
+func (d dollars) String() string {
+	return fmt.Sprintf("$%.2f", d)
+}
+
+type database struct {
+	mu sync.Mutex
+	db map[string]dollars //simulate the database with a map
+
+}
+
+func (d *database) list(w http.ResponseWriter, r *http.Request) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	for item, price := range d.db {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+
+func (d *database) add(w http.ResponseWriter, req *http.Request) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	item := req.URL.Query().Get("item")
+	price := req.URL.Query().Get("price")
+
+	//look up the item to see if it already exists, we don't want to add duplicates.
+	if _, ok := d.db[item]; ok {
+		msg := fmt.Sprintf("duplicate item: %q", item)
+		http.Error(w, msg, http.StatusBadRequest) //send a 400
+		return
+	}
+
+	//convert the string price into a 32 bit float
+	p, err := strconv.ParseFloat(price, 32)
+	if err != nil {
+		msg := fmt.Sprintf("invalid price: %q", price)
+		http.Error(w, msg, http.StatusBadRequest) //send a 400
+		return
+	}
+
+	d.db[item] = dollars(p) //cast the float to the dollars type
+
+	fmt.Fprintf(w, "added %s with price %s\n", item, d.db[item]) //could return a different response code to show we created something
+}
+
+func (d *database) update(w http.ResponseWriter, req *http.Request) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	item := req.URL.Query().Get("item")
+	price := req.URL.Query().Get("price")
+
+	//if the item doesn't exist, we can't update it.
+	if _, ok := d.db[item]; !ok {
+		msg := fmt.Sprintf("item not found: %q", item)
+		http.Error(w, msg, http.StatusNotFound) //send a 404
+		return
+	}
+
+	//convert the string price into a 32 bit float
+	p, err := strconv.ParseFloat(price, 32)
+	if err != nil {
+		msg := fmt.Sprintf("invalid price: %q", price)
+		http.Error(w, msg, http.StatusBadRequest) //send a 400
+		return
+	}
+
+	d.db[item] = dollars(p) //cast the float to the dollars type
+
+	fmt.Fprintf(w, "updated %s with price %s\n", item, d.db[item]) //could return a different response code to show we created something
+}
+
+func (d *database) read(w http.ResponseWriter, req *http.Request) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	item := req.URL.Query().Get("item")
+
+	if _, ok := d.db[item]; !ok {
+		msg := fmt.Sprintf("item not found %q", item)
+		http.Error(w, msg, http.StatusNotFound) //404
+		return
+	}
+
+	fmt.Fprintf(w, "item %s has price %s\n", item, d.db[item])
+}
+
+func (d *database) delete(w http.ResponseWriter, req *http.Request) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	item := req.URL.Query().Get("item")
+
+	if _, ok := d.db[item]; !ok {
+		msg := fmt.Sprintf("item not found %q", item)
+		http.Error(w, msg, http.StatusNotFound) //404
+		return
+	}
+
+	delete(d.db, item)
+	fmt.Fprintf(w, "deleted %s\n", item)
+}
+
+func main() {
+	db := database{
+		db: map[string]dollars{
+			"shoes": 50,
+			"socks": 5,
+		},
+	}
+
+	//add routes
+	http.HandleFunc("/list", db.list) //handlefunc wants a handler
+	http.HandleFunc("/add", db.add)
+	http.HandleFunc("/update", db.update)
+	http.HandleFunc("/read", db.read)
+	http.HandleFunc("/delete", db.delete)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
